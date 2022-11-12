06ed9cc229909fe4061e766ac69968e00dfeeb3a
Justin Ng <ngjustin2002@gmail.com>
2022-11-11 22:22:58 -0800

    struct includes pin assignments and successfully tested firmware

--- max6675.c
+++ max6675.c
@@ -1,26 +1,27 @@
-#include "max6675.h" 
+#include "max6675.h"
 
-max6675_tc* tc_create(void) {
-    return (max6675_tc*)malloc(sizeof(max6675_tc)); 
+max6675_tc* tc_init(GPIO_TypeDef* CS_PORT, uint16_t CS_PIN) {
+    max6675_tc* sensor = (max6675_tc*)malloc(sizeof(max6675_tc));
+    sensor->CS_PORT = CS_PORT;
+    sensor->CS_PIN = CS_PIN;
+    return sensor;
 }
 
-float tc_readTemp(max6675_tc* sensor, GPIO_TypeDef* CS_PORT, uint16_t CS_PIN) {
-    uint8_t buffer[2]; 
-    uint16_t rawTempValue; 
+float tc_readTemp(max6675_tc* sensor) {
+    uint8_t buffer[2];
+    uint16_t rawValue;
 
-    HAL_GPIO_WritePin(CS_PORT, CS_PIN, GPIO_PIN_RESET); 
-    HAL_SPI_Receive(&SPI_TYPEDEF, buffer, 1, HAL_MAX_DELAY); 
-    HAL_GPIO_WritePin(CS_PORT, CS_PIN, GPIO_PIN_SET); 
+    HAL_GPIO_WritePin(sensor->CS_PORT, sensor->CS_PIN, GPIO_PIN_RESET);
+    HAL_SPI_Receive(&hspi1, buffer, 1, HAL_MAX_DELAY);
+    HAL_GPIO_WritePin(sensor->CS_PORT, sensor->CS_PIN, GPIO_PIN_SET);
 
-    rawValue = (buffer[0] | (buffer[1] << 8)) >> 3; 
-    sensor->rawTemp = rawValue; 
+    rawValue = (buffer[0] | (buffer[1] << 8)) >> 3;
+    float celcius = rawValue * HIGH_TEMP_C / DATA_OUTPUT_RES;
+    sensor->tempCelcius = celcius;
 
-    float celcius = rawValue * HIGH_TEMP_C / DATA_OUTPUT_RES; 
-    sensor->tempCelcius = celcius; 
-
-    return celcius; 
+    return celcius;
 }
 
-void tc_free(max6675* sensor) {
-    free(sensor); 
+void tc_free(max6675_tc* sensor) {
+    free(sensor);
 }
--- max6675.h
+++ max6675.h
@@ -1,30 +1,27 @@
-#ifndef MAX6675_H_ 
-#define MAX6675_H_ 
+#ifndef MAX6675_H_
+#define MAX6675_H_
 
 #include "stdlib.h"
 #include "stdbool.h"
 #include "stm32f7xx_hal.h"
 
-
-extern SPI_HandleTypeDef hspi1; 
-#define SPI_TYPEDEF hspi1; 
+extern SPI_HandleTypeDef hspi1;
+//#define SPI_TYPEDEF hspi1;
 
 #define HIGH_TEMP_C 1024 // maximum temperature reading +1024 celcius
 #define DATA_OUTPUT_RES 4096 // 12 bit resolution output
 
-
-typedef struct max6675_tc max6675_tc; 
+typedef struct max6675_tc max6675_tc;
 
 struct max6675_tc {
-    float tempCelcius; 
-    uint16_t rawTemp; 
-    float tc_readTemp(max6675_tc* sensor, GPIO_TypeDef* CS_PORT, uint16_t CS_PIN); 
+    float tempCelcius;
+    GPIO_TypeDef* CS_PORT;
+    uint16_t CS_PIN;
 };
 
+max6675_tc* tc_init(GPIO_TypeDef* CS_PORT, uint16_t CS_PIN);
+float tc_readTemp(max6675_tc* sensor);
+void tc_clean(max6675_tc* sensor);
 
-max6675_tc* tc_create(void); 
-float tc_readTemp(max6675_tc* sensor, GPIO_TypeDef* CS_PORT, uint16_t CS_PIN); 
-void tc_clean(max6675_tc* sensor); 
 
-
-#endif 
+#endif
